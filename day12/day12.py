3f4fb5607d7bb8b57bdbbbbd5caa5f1c1f42344e
Lucas <lucas@mightyhive.com>
2020-12-17 21:20:53 +0000

    day 12 done

---
+++ day12/day12.py
@@ -0,0 +1,50 @@
+import re
+from collections import deque
+DATA = [re.findall('(\w)(\d+)',x.strip())[0] for x in open('./day12.input').readlines()]
+
+d = 0
+f = 'E'
+s = deque(list('ESWN'))
+
+for i in DATA:
+    if i[0] in 'ES':
+        d += int(i[1])
+    elif i[0] in 'WN':
+        d -= int(i[1])
+    elif i[0] == 'F':
+        d += int(i[1]) if f in 'ES' else 0
+        d -= int(i[1]) if f in 'WN' else 0
+    elif i[0] == 'L':
+        s.rotate(int(i[1])//90)
+        f = s[0]
+    elif i[0] == 'R':
+        s.rotate(-int(i[1])//90)
+        f = s[0]
+
+print(abs(d))
+
+wx = 10
+wy = 1
+d = 0
+for i in DATA:
+    arg = int(i[1])
+    if i[0] == 'F':
+        d += (wx + wy) * arg
+    elif i[0] == 'N':
+        wy += arg
+    elif i[0] == 'S':
+        wy -= arg
+    elif i[0] == 'E':
+        wx += arg
+    elif i[0] == 'W':
+        wx -= arg
+    elif i[0] == 'L':
+        while arg:
+            wx, wy = -wy, wx
+            arg -= 90
+    elif i[0] == 'R':
+        while arg:
+            wx, wy = wy, -wx
+            arg -= 90     
+
+print(abs(d))
